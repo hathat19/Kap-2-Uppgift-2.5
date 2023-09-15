2b80c81fc7312729d9bd5d686cedad15df0973b9
humy06001 <humy06001@E-PW0234KS>
2023-09-15 10:59:01 +0200

    Add project files.

---
+++ Kap 2 Uppgift 2.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ber användaren att skriva in en ekvation med två tal och ett gångertecken
+            Console.WriteLine("Skriv en beräkning med två tal och ett gångertecken (a*b).");
+            string calc = Console.ReadLine();
+
+            //Räknar ut index för gångertecknet
+            int multIndex = calc.IndexOf("*");
+
+            //Skapar float-variabler för talen som användaren skrev
+            float number1 = float.Parse(calc[..multIndex]);
+            float number2 = float.Parse(calc[(multIndex + 1)..]);
+
+            //Skriver ut produkten
+            Console.WriteLine($"{calc} = {number1*number2}.");
+            Console.ReadKey();
+        }
+    }
+}
